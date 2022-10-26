53ae84fa0a5ad695be8cc1530da2a6b9f66b2142
Ujith Nimantha <ujithnimantha@gmail.com>
2022-10-26 20:53:07 +0300

    Inbox and Mail icons removed

--- src/components/header/SideBar.js
+++ src/components/header/SideBar.js
@@ -11,8 +11,6 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import LocalMallIcon from '@mui/icons-material/LocalMall';
 import ShoppingCartCheckoutIcon from '@mui/icons-material/ShoppingCartCheckout';
